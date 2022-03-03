0b0a64e821c25c1cffcd3204f954326786d8a95b
MounishaThondaladinne-WAL <mounisha.t@westagilelabs.com>
2022-03-03 16:59:38 +0530

    Adding files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoMainApp from './AssignmentMarch3/TodoMainApp';
+import './AssignmentMarch3/TodoApp.css'
+function App() {
+  return (
+    <div className="App">
+      <TodoMainApp/>
+    </div>
+  );
+}
+export default App;
